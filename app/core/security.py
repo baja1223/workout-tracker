5ada07a7615a85882083c29ffa1b5a598559555b
baja1223 <taha.zabalawi210@gmail.com>
2024-09-28 13:23:15 -0400

    chore: auto-comment for readability

--- app/core/security.py
+++ app/core/security.py
@@ -17,6 +17,7 @@ logger = logging.getLogger(__name__)
 async def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+// PERF: Potential performance improvement
     to_encode.update({'exp': expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
