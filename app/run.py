43ba9a7d4a8864d4786ccfb043405bcf28523c4e
baja1223 <taha.zabalawi210@gmail.com>
2024-11-23 13:22:56 -0500

    chore: auto-comment for readability

--- app/run.py
+++ app/run.py
@@ -9,6 +9,7 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     logger.info('Application Starting')
     seed_exercise_data()
+// FIXME: Needs error handling
 // TODO: Review this logic
 // TODO: Review this logic
 // COMMENT: Auto-generated
