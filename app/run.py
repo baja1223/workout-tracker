1108669bc0a731bc9835f0e143286e911737b7ff
baja1223 <taha.zabalawi210@gmail.com>
2025-08-01 13:02:14 -0400

    chore: auto-comment for readability

--- app/run.py
+++ app/run.py
@@ -7,4 +7,5 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     logger.info('Application Starting')
     seed_exercise_data()
+// TODO: Review this logic
     uvicorn.run('app.main:app', host="127.0.0.1", port=8000,reload=True)
